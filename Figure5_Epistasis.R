5e57943690c30380dd6b1e19e20863a3cf7869cb
Alexander Yang <alecander.yang@gmail.com>
2021-03-06 17:33:24 -0500

    Update Figure5_Epistasis.R

--- Figure5_Epistasis.R
+++ Figure5_Epistasis.R
@@ -24,7 +24,7 @@ for(x in SPOP_list){
   gene1_after_gene2 <- unlist(c(as.character("gene1_after_gene2"), as.numeric(PRAD_epistasis[x,5] - PRAD_epistasis[x,3])))
   gene2_after_gene1 <- unlist(c(as.character("gene2_after_gene1"), as.numeric(PRAD_epistasis[x,6] - PRAD_epistasis[x,4])))
   gene1_after_gene2[1] <- str_replace(gene1_after_gene2[1], "gene1", as.character(PRAD_epistasis[x,1]))
-  gene1_after_gene2[1] <- str_replace(gene1_after_gene2[1], "gene2", as.character (PRAD_epistasis[x,2]))
+  gene1_after_gene2[1] <- str_replace(gene1_after_gene2[1], "gene2", as.character(PRAD_epistasis[x,2]))
   gene2_after_gene1[1] <- str_replace(gene2_after_gene1[1], "gene1", as.character(PRAD_epistasis[x,1]))
   gene2_after_gene1[1] <- str_replace(gene2_after_gene1[1], "gene2", as.character(PRAD_epistasis[x,2]))
   epistatic_change_SPOP <- rbind(epistatic_change_SPOP, gene1_after_gene2, gene2_after_gene1)
@@ -62,6 +62,7 @@ waterfall_SPOP <- ggplot(epistatic_change_SPOP, aes(x= reorder(gene, change), y=
   scale_y_continuous(labels = scientific, breaks = c(-1e4, 0, 1e4), limits = c(-2e4, 2e4))
 
 waterfall_SPOP
+ggsave("PRAD_figures/epistasis/Top 25/waterfall_SPOP.png", width = 10, dpi=300, height = 7)
 
 ###TP53###
 
@@ -98,12 +99,19 @@ epistatic_change_TP53_after[,1] <- sub("after_TP53", "", epistatic_change_TP53_a
 #Blank spot
 blank <- data.frame(gene = "BLANK", change = -5000, time = "Before")
 
-epistatic_change_TP53 <- rbind(epistatic_change_TP53_before, epistatic_change_TP53_after)
+epistatic_change_TP53 <- rbind(epistatic_change_TP53_before, blank, epistatic_change_TP53_after)
 
-gene_labels <- c("SPOP", "CTNNB1", "KMT2D", "AR", "FOXA1", "MUC16", "KMT2D", "SYNE1", "KMT2C",
-                 "FOXA1", "SPOP", "CTNNB1", "SYNE1", "AR", "KMT2C", "MUC16")
+epistatic_change_TP53$gene <- factor(epistatic_change_TP53$gene, levels = c("KMT2D", "AR", "MUC16",
+                                                                            "FOXA1", "SPOP", "CTNNB1",
+                                                                            "SYNE1", "KMT2C", "BLANK",
+                                                                            "SPOP_", "CTNNB1_", "FOXA1_",
+                                                                            "KMT2D_", "SYNE1_", "KMT2C_",
+                                                                            "AR_", "MUC16_"))
 
-waterfall_TP53 <- ggplot(epistatic_change_TP53, aes(x= reorder(gene, change), y=change, fill=time)) +
+gene_labels <- c("KMT2D", "AR", "MUC16", "FOXA1", "SPOP", "CTNNB1", "SYNE1", "KMT2C", "",
+                 "SPOP", "CTNNB1", "FOXA1", "KMT2D", "SYNE1", "KMT2C", "AR", "MUC16")
+
+waterfall_TP53 <- ggplot(epistatic_change_TP53, aes(x= gene, y=change, fill=time)) +
   geom_bar(stat = "identity", position = "dodge") + theme_classic() +
   scale_fill_manual(values = c("#F8766D","#00BFC4"))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
@@ -113,3 +121,59 @@ waterfall_TP53 <- ggplot(epistatic_change_TP53, aes(x= reorder(gene, change), y=
   scale_y_continuous(labels = scientific, breaks = c(-1e4, 0, 1e4), limits = c(-2e4, 2e4))
 
 waterfall_TP53
+ggsave("PRAD_figures/epistasis/Top 25/waterfall_TP53.png", width = 6, dpi=300, height = 5)
+
+
+###AR###
+
+#Select your gene pairs of interest
+AR_list <- c(27)
+
+epistatic_change_AR <- c()
+
+#Decoupling the gene pairs
+for(x in AR_list){
+  gene1_after_gene2 <- unlist(c(as.character("gene1_after_gene2"), as.numeric(PRAD_epistasis[x,5] - PRAD_epistasis[x,3])))
+  gene2_after_gene1 <- unlist(c(as.character("gene2_after_gene1"), as.numeric(PRAD_epistasis[x,6] - PRAD_epistasis[x,4])))
+  gene1_after_gene2[1] <- str_replace(gene1_after_gene2[1], "gene1", as.character(PRAD_epistasis[x,1]))
+  gene1_after_gene2[1] <- str_replace(gene1_after_gene2[1], "gene2", as.character(PRAD_epistasis[x,2]))
+  gene2_after_gene1[1] <- str_replace(gene2_after_gene1[1], "gene1", as.character(PRAD_epistasis[x,1]))
+  gene2_after_gene1[1] <- str_replace(gene2_after_gene1[1], "gene2", as.character(PRAD_epistasis[x,2]))
+  epistatic_change_AR <- rbind(epistatic_change_AR, gene1_after_gene2, gene2_after_gene1)
+}
+
+epistatic_change_AR <- data.frame(gene = epistatic_change_AR[,1], change = as.numeric(epistatic_change_AR[,2]))
+
+#Separating "Before" and "After"
+epistatic_change_AR_before <- epistatic_change_AR[grep("AR_", epistatic_change_AR[,1]),]
+epistatic_change_AR_before$time <- rep("Before", 1)
+epistatic_change_AR_before <- epistatic_change_AR_before[order(-epistatic_change_AR_before$change),]
+epistatic_change_AR_after <- epistatic_change_AR[grep("_AR", epistatic_change_AR[,1]),]
+epistatic_change_AR_after$time <- rep("After", 1)
+epistatic_change_AR_after <- epistatic_change_AR_after[order(-epistatic_change_AR_after$change),]
+
+#Need to have extra underscore to have unique names
+epistatic_change_AR_before[,1] <- sub("AR_after_", "", epistatic_change_AR_before[,1])
+epistatic_change_AR_after[,1] <- sub("after_AR", "", epistatic_change_AR_after[,1])
+
+#Blank spot
+blank <- data.frame(gene = "BLANK", change = 0, time = "Before")
+
+epistatic_change_AR <- rbind(epistatic_change_AR_before, blank, epistatic_change_AR_after)
+
+epistatic_change_AR$gene <- factor(epistatic_change_AR$gene, levels = c("MUC16", "BLANK", "MUC16_"))
+
+gene_labels <- c("MUC16", "", "MUC16")
+
+waterfall_AR <- ggplot(epistatic_change_AR, aes(x= gene, y=change, fill=time)) +
+  geom_bar(stat = "identity", position = "dodge") + theme_classic() +
+  scale_fill_manual(values = c("#F8766D","#00BFC4"))+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        legend.position = "bottom", legend.title = element_blank()) +
+  ggtitle("AR-MUC16") + xlab("Gene") + ylab ("Epistatic change in selection")+
+  scale_x_discrete(labels = gene_labels) + scale_fill_discrete(breaks = c("Before", "After"))+
+  scale_y_continuous(labels = scientific, breaks = c(-1e4, 0, 1e4, 2e4, 3e4), limits = c(-1e4, 4e4))
+
+waterfall_AR
+ggsave("PRAD_figures/epistasis/Top 25/waterfall_AR.png", width = 3, dpi=300, height = 6)
+
